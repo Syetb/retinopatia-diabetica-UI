d44a7b0e7bdb779045af945c37efc5e1579913f8
Betsy Lincango <syetb@live.com.mx>
2025-12-16 21:32:53 -0500

    add init proyect

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="UIDE", layout="centered")
+
+st.title("Desarrollo de un Modelo de Clasificación para la Detección Temprana de la Retinopatía Diabética mediante Aprendizaje Automático y Procesamiento de Imágenes Retinales")
